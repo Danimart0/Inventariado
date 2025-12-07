57c6a1d1a3b8907df95bd1eefba7c26f18130fd7
Daniel <Daniel>
2025-12-07 15:52:09 -0600

    Fix: Agregar 0.0.0.0 a ALLOWED_HOSTS para corregir error de servidor

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1up7zj(z+c#ezvh)yk&^6l-_3zf%j&c7&j+c2y+xy%cvwa!z19
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
